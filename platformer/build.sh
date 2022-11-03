a3e315543ff4337117991abc26acf242cb7af717
cubbit <16192627+aratama@users.noreply.github.com>
2022-11-03 20:27:05 +0900

    gh pages

--- platformer/build.sh
+++ platformer/build.sh
@@ -1,2 +1,4 @@
 cargo build --release
 wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code target/wasm32-unknown-unknown/release/cart.wasm > cart.min.wasm
+wasm-opt -Oz --zero-filled-memory --strip-producers --dce cart.min.wasm -o cart.min.wasm
+w4 bundle cart.min.wasm --title "mygame" --html ../docs/mygame.html
