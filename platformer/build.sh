18988f136d22e2a6b8e8e7b7f07a6b77bdabcfb0
aratama <16192627+aratama@users.noreply.github.com>
2022-11-08 01:54:01 +0900

    use bytes

--- platformer/build.sh
+++ platformer/build.sh
@@ -2,7 +2,10 @@ rm -r target
 cargo build --release
 wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code target/wasm32-unknown-unknown/release/cart.wasm > target/wasm32-unknown-unknown/release/cart.min.wasm
 npx wasm-opt -Oz --zero-filled-memory --strip-producers --dce target/wasm32-unknown-unknown/release/cart.min.wasm -o target/wasm32-unknown-unknown/release/cart.min.wasm
+
+# Error: Section metadata not found
 # npx @webassemblyjs/wasm-strip target/wasm32-unknown-unknown/release/cart.min.wasm
+
 ls -l target/wasm32-unknown-unknown/release/cart.wasm
 ls -l target/wasm32-unknown-unknown/release/cart.min.wasm
 w4 bundle target/wasm32-unknown-unknown/release/cart.min.wasm --title "mygame" --html ../docs/mygame.html
--- platformer/src/save.rs
+++ platformer/src/save.rs
@@ -1,5 +1,14 @@
+use crate::wasm4::{diskr, diskw};
+use bytes::{Buf, BufMut};
+use std::mem::size_of;
+
 pub const GAME_DATA_VERSION: u8 = 0;
-pub const GAME_DATA_SIZE: usize = 16;
+
+#[rustfmt::skip]
+pub const GAME_DATA_SIZE: usize = 
+    size_of::<u8>() + // version
+    size_of::<f32>() + // x
+    size_of::<f32>(); // y
 
 #[derive(Clone, Copy)]
 pub struct GameData {
@@ -8,12 +17,25 @@ pub struct GameData {
     pub y: f32,
 }
 
-pub fn save(_game_data: &GameData) {}
+pub fn save(game_data: &GameData) {
+    let mut buf = Vec::new();
+    buf.put_u8(0); // version
+    buf.put_f32_le(game_data.x); // x
+    buf.put_f32_le(game_data.y); // y
+    let bytes: &[u8] = buf.as_slice().try_into().unwrap();
+    unsafe {
+        diskw(bytes.as_ptr(), core::mem::size_of_val(bytes) as u32);
+    }
+}
 
 pub fn load() -> GameData {
-    GameData {
-        version: 0,
-        x: 0.0,
-        y: 0.0,
+    let mut buffer: [u8; GAME_DATA_SIZE] = [0; GAME_DATA_SIZE];
+    unsafe {
+        diskr(buffer.as_mut_ptr(), buffer.len() as u32);
     }
+    let mut p = &buffer[..];
+    let version = p.get_u8();
+    let x = p.get_f32_le();
+    let y = p.get_f32_le();
+    GameData { version, x, y }
 }
