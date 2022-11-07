8f744261b3bd788085a6a1b6cfe0b3fe4bbea2e3
aratama <16192627+aratama@users.noreply.github.com>
2022-11-08 01:23:03 +0900

    remove serde

--- platformer/src/save.rs
+++ platformer/src/save.rs
@@ -1,37 +1,19 @@
-use crate::wasm4::{diskr, diskw, trace};
-use serde::{Deserialize, Serialize};
-use std::convert::TryInto;
-
 pub const GAME_DATA_VERSION: u8 = 0;
 pub const GAME_DATA_SIZE: usize = 16;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Clone, Copy)]
 pub struct GameData {
     pub version: u8,
     pub x: f32,
     pub y: f32,
 }
 
-pub fn save(game_data: &GameData) {
-    let vec: Vec<u8> = rmp_serde::to_vec(game_data).unwrap();
-    let bytes: &[u8] = vec.as_slice().try_into().unwrap();
-    unsafe {
-        diskw(bytes.as_ptr(), core::mem::size_of_val(bytes) as u32);
-    }
-    if GAME_DATA_SIZE < core::mem::size_of_val(bytes) {
-        trace(format!(
-            "[WARNING] save data size {} exceeds read buffer size {}",
-            core::mem::size_of_val(bytes) as u32,
-            GAME_DATA_SIZE
-        ));
-    }
-}
+pub fn save(_game_data: &GameData) {}
 
 pub fn load() -> GameData {
-    let mut buffer: [u8; GAME_DATA_SIZE] = [0; GAME_DATA_SIZE];
-
-    unsafe {
-        diskr(buffer.as_mut_ptr(), buffer.len() as u32);
+    GameData {
+        version: 0,
+        x: 0.0,
+        y: 0.0,
     }
-    rmp_serde::from_slice(&buffer).unwrap()
 }
