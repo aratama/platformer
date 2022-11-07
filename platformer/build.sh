0c7b4e95f6a43fb2549a3ec391a1e777c7cf6f35
aratama <16192627+aratama@users.noreply.github.com>
2022-11-08 01:03:14 +0900

    tweak build script

--- platformer/build.sh
+++ platformer/build.sh
@@ -1,4 +1,8 @@
+rm -r target
 cargo build --release
 wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code target/wasm32-unknown-unknown/release/cart.wasm > target/wasm32-unknown-unknown/release/cart.min.wasm
 npx wasm-opt -Oz --zero-filled-memory --strip-producers --dce target/wasm32-unknown-unknown/release/cart.min.wasm -o target/wasm32-unknown-unknown/release/cart.min.wasm
+# npx @webassemblyjs/wasm-strip target/wasm32-unknown-unknown/release/cart.min.wasm
+ls -l target/wasm32-unknown-unknown/release/cart.wasm
+ls -l target/wasm32-unknown-unknown/release/cart.min.wasm
 w4 bundle target/wasm32-unknown-unknown/release/cart.min.wasm --title "mygame" --html ../docs/mygame.html
