081111a4b2b91ef46070b1af76d0601f3fdfc6f1
cubbit <16192627+aratama@users.noreply.github.com>
2022-11-24 02:01:12 +0900

    update netplay

--- src/game.rs
+++ src/game.rs
@@ -1,27 +1,39 @@
 use crate::input::Inputs;
 
-use crate::scene::game_scene::GameScene;
 use crate::scene::title_scene::TitleScene;
 use crate::scene::Scene;
 use crate::sound::update_bgm;
 use crate::wasm4;
 
 pub struct Game {
-    prev_gamepad: u8,
+    prev_gamepad1: u8,
+    prev_gamepad2: u8,
+    prev_gamepad3: u8,
+    prev_gamepad4: u8,
     scene: Scene,
+
+    player2_active: bool,
+    player3_active: bool,
+    player4_active: bool,
 }
 
 impl Game {
     pub fn new() -> Self {
         Game {
-            prev_gamepad: 0,
+            prev_gamepad1: 0,
+            prev_gamepad2: 0,
+            prev_gamepad3: 0,
+            prev_gamepad4: 0,
             scene: Scene::TitleScene(TitleScene::new()),
+            player2_active: false,
+            player3_active: false,
+            player4_active: false,
         }
     }
 
     pub fn update(&mut self) {
         let gamepad = unsafe { *wasm4::GAMEPAD1 };
-        let inputs = Inputs::new(gamepad, self.prev_gamepad);
+        let inputs = Inputs::new(gamepad, self.prev_gamepad1);
         let result = match { &mut self.scene } {
             Scene::TitleScene(t) => t.update(&inputs),
             Scene::GameScene(g) => g.update(&inputs),
@@ -31,7 +43,7 @@ impl Game {
             Option::None => {}
             Option::Some(next) => self.scene = next,
         }
-        self.prev_gamepad = unsafe { *wasm4::GAMEPAD1 };
+        self.prev_gamepad1 = unsafe { *wasm4::GAMEPAD1 };
 
         update_bgm();
     }
--- src/netplay.rs
+++ src/netplay.rs
@@ -3,8 +3,7 @@ pub const NETPLAY: *const u8 = 0x0020 as *const u8;
 pub fn get_my_player_index() -> u8 {
     unsafe {
         if is_netplay_active() {
-            let player_idx = *NETPLAY & 0b011;
-            player_idx
+            *NETPLAY & 0b011
         } else {
             0
         }
